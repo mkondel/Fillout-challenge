128d942e3108a4055f162b8b1ec49c391910b612
Max <mkondel@gmail.com>
2025-06-26 22:11:44 +0000

    initial button classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import { useState } from "react";
 import { IconCheck, IconInfo, IconPage, IconPlus } from "./components/Icons";
 import Dashes from "./components/Dashes";
 import Button from "./components/Button";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const addPage = () => alert("User clicked to add a page");
 
   return (
     <main className="flex flex-col gap-8 px-4 py-16 max-w-[1280px] mx-auto">
       <div className="flex items-center justify-center">
-        <Button text="Info" Icon={IconInfo} variant="active" />
-        <Dashes />
-        <Button text="Details" Icon={IconPage} variant="complete" />
-        <Dashes />
-        <Button text="Other" Icon={IconPage} variant="complete" />
-        <Dashes />
-        <Button text="Ending" Icon={IconCheck} variant="complete" />
-        <Dashes />
-        <Button text="Add page" Icon={IconPlus} variant="active" />
+        helloooo
+        <Button text="Info" Icon={IconInfo} />
+        <Dashes onClickPlus={addPage} />
+        <Button text="Details" Icon={IconPage} />
+        <Dashes onClickPlus={addPage} />
+        <Button text="Other" Icon={IconPage} />
+        <Dashes onClickPlus={addPage} />
+        <Button text="Ending" Icon={IconCheck} />
+        <Dashes onClickPlus={addPage} showPlus={false} />
+        <button onClick={addPage} className="btn btn--unchanging">
+          <IconPlus />
+          Add page
+        </button>
       </div>
     </main>
   );
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,14 @@
-const Button = ({ text, Icon, variant }) => (
-  <button className={`btn btn--${variant}`}>
-    <Icon />
+import { IconHamburger } from "./Icons";
+
+const Button = ({ text, Icon }) => (
+  <button className={`btn btn--complete group`}>
+    <div className={`stroke-[#8C93A1] group-focus:stroke-[#F59D0E]`}>
+      <Icon />
+    </div>
     {text}
+    <div className="hidden group-focus:block">
+      <IconHamburger />
+    </div>
   </button>
 );
 
