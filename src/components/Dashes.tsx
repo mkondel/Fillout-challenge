b8106e5490a2170fed6d75948fcb387528caf562
Max <mkondel@gmail.com>
2025-06-27 23:48:56 +0000

    found the transform I was looking for

--- src/components/Dashes.tsx
+++ src/components/Dashes.tsx
@@ -6,14 +6,14 @@ const Dashes = ({ showPlus = true, onClickPlus = () => {} }) => {
       {showPlus && (
         <div
           onClick={onClickPlus}
-          className="transition duration-200 bg-white border w-fit flex items-center justify-center opacity-0 group-hover:opacity-100 absolute rounded-full cursor-pointer"
+          className="transform duration-100 bg-white border w-fit flex items-center justify-center opacity-0 group-hover:opacity-100 absolute rounded-full cursor-pointer"
         >
           <IconPlus classNameCustom={"p-0.5"} />
         </div>
       )}
       <div
         className={`-mx-5 ${
-          showPlus && "group-hover:m-0"
+          showPlus && "transform duration-300 group-hover:m-0"
         } -z-[1] relative -top-0.5`}
       >
         ----------
